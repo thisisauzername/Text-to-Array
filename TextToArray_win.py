10e8500a7916cfad1fa0bc770059b12ca94a3c19
Uzair Tariq <uzair0213@icloud.com>
2018-10-09 21:02:07 -0600

    Clipboard Functionality
    Added copying functionality

--- TextToArray_win.py
+++ TextToArray_win.py
@@ -4,14 +4,7 @@
 # Paste in your long list of text, with all seperate words seperated with a
 # space
 #------------------------------------------------------------------------------#
-import sys
-import subprocess
-
-def write_to_clipboard(output):
-    if sys.platform == 'win32' or sys.platform == 'cygwin':
-        subprocess.Popen(['clip'], stdin=subprocess.PIPE).communicate(output)
-    else:
-        raise Exception('Platform not supported')
+import clipboard
 
 print("(c) 2018 Uzair Tariq \nPlain text to array program \n")
 
@@ -30,7 +23,7 @@ print(array)
 copy = input("\nWould you like to copy the result? (y/n) \n")
 
 if copy == 'y':
-    write_to_clipboard(array)
+    clipboard.copy(array)
     print("\nCopied to clipboard! \n")
 elif copy == 'n':
     '\n'
