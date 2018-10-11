95b596b4f994f1a62d4467d269ac580d848cd29c
Uzair Tariq <uzair0213@icloud.com>
2018-10-11 10:42:11 -0600

    Small issue fixed
    A small issue where the first and last strings in an array if the user selected y for string_or_not were missing an apostrophe.

--- TextToArray_mac.py
+++ TextToArray_mac.py
@@ -27,7 +27,10 @@ else:
 
 print("\nHere's your array: \n")
 
-array = '[' + plain_text + ']'
+if string_or_not == 'y':
+    array = "['" + plain_text + "']"
+else:
+    array = '[' + plain_text + ']'
 
 print(array)
 
